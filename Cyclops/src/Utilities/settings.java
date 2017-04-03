e0043bb824fe6e3590276d54dad5e53242261475
Sandra John <sandra.john@cuelogic.co.in>
2017-04-03 21:57:05 +0530

    Create settings.java

--- Cyclops/src/Utilities/settings.java
+++ Cyclops/src/Utilities/settings.java
@@ -9,7 +9,7 @@ public static String cyclops_url() throws Exception
    {
 	  try
 	  {
-		String url = "https://sales-stg.travelpass.com/?vocalcomid=101078@nat.ipitimi.net&did=1234567890&indexid=6453654736&ani=7453754777" ;
+		String url = "https://sales-stg.travelpass.com/?vocalcomid={agent_id}@nat.ipitimi.net&did=1234567890&indexid=6453654736&ani=7453754777" ;
 		return url;
 	  }
 	  catch (Exception e)
