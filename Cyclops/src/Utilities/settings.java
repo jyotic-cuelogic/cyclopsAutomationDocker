2aa96f66527e6f7602117839e13b748eb35b428b
Sandra John <sandra.john@cuelogic.co.in>
2017-04-04 20:16:05 +0530

    Changed URL value

--- Cyclops/src/Utilities/settings.java
+++ Cyclops/src/Utilities/settings.java
@@ -9,7 +9,7 @@ public static String cyclops_url() throws Exception
    {
 	  try
 	  {
-		String url = "https://sales.travelpass.com/?vocalcomid=101382@nat.ipitimi.net&did=1234567890&indexid=6453654736&ani=7453754777" ;
+		String url = Cyclops_staging_url;
 		return url;
 	  }
 	  catch (Exception e)
